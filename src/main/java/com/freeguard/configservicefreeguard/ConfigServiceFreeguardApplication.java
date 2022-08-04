8e19ca11fdbeac8c8a87b418c38a7e369902db55
OscarRamirez <al221310583@gmail.com>
2022-08-04 18:17:33 -0500

    :wrench: Initital commit

---
+++ src/main/java/com/freeguard/configservicefreeguard/ConfigServiceFreeguardApplication.java
@@ -0,0 +1,15 @@
+package com.freeguard.configservicefreeguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceFreeguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServiceFreeguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freeguard/configservicefreeguard/ConfigServiceFreeguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeguard.configservicefreeguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceFreeguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
